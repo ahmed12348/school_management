c8e025c04da064217e99e560b765c5d05abdd386
Ahmed Mahmoud <60364682+ahmed12348@users.noreply.github.com>
2022-04-04 17:09:01 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,9 @@ use App\Http\Controllers\API\StudentController;
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 Route::middleware('auth:api')->group( function () {
     Route::resource('students', StudentController::class);
 });
